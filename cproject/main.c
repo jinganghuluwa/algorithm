76e64499e3a56c716b2a0daf04b635ba814f7eba
tongzhichao <tongzhichao@le.com>
2017-06-19 18:11:29 +0800

    iii
    Signed-off-by: tongzhichao <tongzhichao@le.com>

--- cproject/main.c
+++ cproject/main.c
@@ -3,6 +3,47 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a[] = {87,-67,35,7,4,-34,765,-34,6,5,-4,6,43,-87,76};
+    int b = maxsubsum(a,0,sizeof(a)/sizeof(int));
+    b=3;
+    printf("b %d",b);
     return 0;
+}
+
+int maxsubsum( const int a[],int left,int right )
+{
+    int maxleft,maxright;
+    int subleft,subright;
+    int maxsubleft,maxsubright;
+    int center,i;
+    if(left==right)
+        if(a[left]>0)
+            return a[left];
+        else
+            return 0;
+    center = (left+right)/2;
+    maxleft = maxsubsum(a,left,center);
+    maxright = maxsubsum(a,center,right);
+    subleft=0;
+    subright=0;
+    for( i=center; i>=left; i--)
+    {
+        subleft+=a[i];
+        if(subleft>maxsubleft)
+            maxsubleft=subleft;
+    }
+    for( i=center; i<=right; i++)
+    {
+        subright+=a[i];
+        if(subright>maxsubright)
+            maxsubright = subright;
+
+    }
+    return maxsubthree(maxleft,maxright,(maxsubleft+maxsubright));
+
+}
+
+int maxsubthree(int a,int b,int c)
+{
+    return a >b?(a>c?a:c):(b>c?b:c);
 }
